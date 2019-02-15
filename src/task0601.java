681b14a755c5ec7510d800c444af00e1d732692f
SChe <dizigin@yandex.ru>
2019-02-15 09:42:46 +0300

    Added task0601

---
+++ src/task0601.java
@@ -0,0 +1,20 @@
+public class task0601 {
+/*
+public class Cat {
+    protected void finalize() throws Throwable {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
+public class Cat {
+    protected void finalize() throws Throwable {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
+*/
+}
