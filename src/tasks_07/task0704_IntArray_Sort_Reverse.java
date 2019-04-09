f54b39335c672d677b1bf7ad8a651966fbee006f
SChe <dizigin@yandex.ru>
2019-04-09 18:54:31 +0300

    Added task0704

---
+++ src/tasks_07/task0704_IntArray_Sort_Reverse.java
@@ -0,0 +1,31 @@
+package tasks_07;
+
+public class task0704_IntArray_Sort_Reverse {
+/*
+Переверни массив
+1. Создать массив на 10 чисел.
+2. Ввести с клавиатуры 10 чисел и записать их в массив.
+3. Вывести на экран элементы массива в обратном порядке, каждое значение выводить с новой строки.
+
+Требования:
+1. Программа должна создавать массив на 10 целых чисел.
+2. Программа должна считывать числа для массива с клавиатуры.
+3. Программа должна выводить 10 строчек, каждую с новой строки.
+4. Массив должен быть выведен на экран в обратном порядке.
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        int [] array = new int[10];
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < array.length; i++){
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+        for (int i = array.length-1; i >= 0; i--){
+            System.out.println(array[i]);
+        }
+    }
+}
+
+*/
+}
