05f24e1b5ab5bd486bf6d60cdd43f8e97965d396
SChe <dizigin@yandex.ru>
2019-05-21 12:39:59 +0300

    Added task0714

---
+++ src/tasks_07/task0714_Del_mass_pos.java
@@ -0,0 +1,30 @@
+package tasks_07;
+
+public class task0714_Del_mass_pos {
+/*
+Слова в обратном порядке
+Введи с клавиатуры 5 слов в список строк. Удали 3 - ий элемент списка, и выведи оставшиеся элементы в обратном порядке.
+
+Требования:
+1. Объяви переменную типа ArrayList<String> (список строк) и сразу проинициализируй ee.
+2. Считай 5 строк с клавиатуры и добавь их в список.
+3. Удали третий элемент списка.
+4. Выведи элементы на экран, каждый с новой строки.
+5. Порядок вывода должен быть обратный.
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        ArrayList<String> list = new ArrayList<String>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < 5; i++){
+            list.add(reader.readLine());
+        }
+        list.remove(2);
+        for (int i = list.size()-1; i >= 0; i--){
+            System.out.println(list.get(i));
+        }
+    }
+}
+*/
+}
