e50930db55cf66157ac6a8f41d3f1a258ef85cae
SChe <dizigin@yandex.ru>
2019-10-28 14:26:43 +0300

    Added task0722

---
+++ src/tasks_07/task0722.java
@@ -0,0 +1,35 @@
+package tasks_07;
+
+public class task0722 {
+/*
+Это конец
+Создать список строк.
+Ввести строки с клавиатуры и добавить их в список.
+Вводить с клавиатуры строки, пока пользователь не введет строку "end". Саму строку "end" не учитывать.
+Вывести строки на экран, каждую с новой строки.
+
+Требования:
+1. Объяви переменную типа список строк и сразу проинициализируй ee.
+2. Считывай строки с клавиатуры и добавляй их в список, пока пользователь не введет строку "end".
+3. Саму строку "end" не нужно добавлять в список.
+4. Выведи список на экран, каждое значение с новой строки.
+5. Используй цикл for.
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        //напишите тут ваш код
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; ; i++){
+            String s = reader.readLine();
+            if (s.equals("end")) break;
+            else list.add(s);
+        }
+        for (int i = 0; i < list.size(); i++){
+            System.out.println(list.get(i));
+
+        }
+    }
+}
+ */
+}
