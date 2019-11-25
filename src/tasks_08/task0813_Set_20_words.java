7188ef2ad4721001e640500c5b29bea9b9e4a917
Chernov <dizigin@yandex.ru>
2019-11-25 16:44:46 +0300

    Added task0813

---
+++ src/tasks_08/task0813_Set_20_words.java
@@ -0,0 +1,47 @@
+package tasks_08;
+
+public class task0813_Set_20_words {
+/*
+20 слов на букву «Л»
+Создать множество строк (Set<String>), занести в него 20 слов на букву "Л".
+
+Требования:
+1. Не изменяй заголовок метода createSet().
+2. Программа не должна выводить текст на экран.
+3. Программа не должна считывать значения с клавиатуры.
+4. Метод createSet() должен создавать и возвращать множество (реализация HashSet).
+5. Множество из метода createSet() должно содержать 20 слов на букву «Л».
+
+public class Solution {
+    public static Set<String> createSet() {
+        //напишите тут ваш код
+    Set<String> set = new HashSet<String>();
+        set.add("Лев");
+        set.add("Лодка");
+        set.add("Лук");
+        set.add("Линька");
+        set.add("Луна");
+        set.add("Лыжи");
+        set.add("Леди");
+        set.add("Лобби");
+        set.add("Левша");
+        set.add("Ладоши");
+        set.add("Лебедка");
+        set.add("Лебедь");
+        set.add("Люк");
+        set.add("Ларек");
+        set.add("Лучник");
+        set.add("Лиса");
+        set.add("Лоток");
+        set.add("Лоскут");
+        set.add("Лаванда");
+        set.add("Ладонь");
+           return set;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+*/
+}
