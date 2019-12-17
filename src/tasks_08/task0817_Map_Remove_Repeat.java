0157fbcdd900d5acd536b42204cd7e963e080c5b
Chernov <dizigin@yandex.ru>
2019-12-17 13:19:30 +0300

    Added task0817

--- src/tasks_08/task0817_Map_Remove_Repeat.java
+++ src/tasks_08/task0817_Map_Remove_Repeat.java
@@ -5,14 +5,12 @@ public class task0817_Map_Remove_Repeat {
 Нам повторы не нужны
 Создать словарь (Map<String, String>) занести в него десять записей по принципу "фамилия" - "имя".
 Удалить людей, имеющих одинаковые имена.
-
 Требования:
 1. Программа не должна выводить текст на экран.
 2. Программа не должна считывать значения с клавиатуры.
 3. Метод createMap() должен создавать и возвращать словарь Map с типом элементов String, String состоящих из 10 записей.
 4. Метод removeTheFirstNameDuplicates() должен удалять из словаря всех людей, имеющие одинаковые имена.
 5. Метод removeTheFirstNameDuplicates() должен вызывать метод removeItemFromMapByValue().
-
 public class Solution {
     public static Map<String, String> createMap() {
         //напишите тут ваш код
