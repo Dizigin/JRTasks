1caeb800244d5232ace09607049fa6f716496599
Chernov <dizigin@yandex.ru>
2019-12-17 13:20:14 +0300

    Added task0808

--- src/tasks_08/task0808_inset_get_set_remove.java
+++ src/tasks_08/task0808_inset_get_set_remove.java
@@ -1,6 +1,6 @@
 package tasks_08;
 
-public class task808_inset_get_set_remove {
+public class task0808_inset_get_set_remove {
 /*
 10 тысяч удалений и вставок
 Для arrayList и linkedList провести 10 тысяч вставок, удалений, а также вызовов get и set.
