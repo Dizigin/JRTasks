44207c7eaa476fea775f5e900612d551a8671277
Chernov <dizigin@yandex.ru>
2020-06-29 18:13:25 +0300

    Added task0921

---
+++ src/tasks_09/task0921_ReadIntData.java
@@ -0,0 +1,43 @@
+package tasks_09;
+
+public class task0921_ReadIntData {
+/*
+Метод в try..catch
+Написать программу, которая будет вводить числа с клавиатуры.
+Код по чтению чисел с клавиатуры должен быть в методе readData.
+Код внутри readData обернуть в try..catch.
+Если пользователь ввёл какой-то текст, вместо ввода числа, то метод должен перехватить исключение и вывести на экран все ранее введенные числа в качестве результата.
+Числа выводить с новой строки сохраняя порядок ввода.
+
+Требования:
+1. Программа должна считывать данные с клавиатуры.
+2. Метод main не изменять.
+3. Метод main не должен кидать исключений.
+4. Метод readData должен содержать блок try..catch.
+5. Если пользователь ввел текст, а не число, программа должна вывести все ранее введенные числа.
+
+public class Solution {
+    public static void main(String[] args) {
+        readData();
+    }
+
+    public static void readData() {
+        List<Integer> list = new ArrayList<>();
+        try {
+
+            while (true){
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                    String s = reader.readLine();
+                    int n = Integer.parseInt(s);
+                    list.add(n);
+            }
+        }
+        catch (Exception e){
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println(list.get(i));
+            }
+        }
+    }
+}
+*/
+}
