daff5c4a92c4009b2b140c8637bb23450f9063ae
Chernov <dizigin@yandex.ru>
2020-06-30 12:30:57 +0300

    Added task0922

---
+++ src/tasks_09/task0922_FormatInputOutputDate.java
@@ -0,0 +1,29 @@
+package tasks_09;
+
+public class task0922_FormatInputOutputDate {
+/*
+Какое сегодня число?
+Ввести с клавиатуры дату в формате "2013-08-18"
+Вывести на экран введенную дату в виде "AUG 18, 2013".
+Воспользоваться объектом Date и SimpleDateFormat.
+
+Требования:
+1. Программа должна считывать данные с клавиатуры.
+2. В программе должна быть объявлена переменная типа SimpleDateFormat.
+3. В программе должна быть объявлена переменная типа Date.
+4. Программа должна выводить данные на экран.
+5. Вывод должен соответствовать заданию.
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        Date date;
+        SimpleDateFormat formatForDateInput = new SimpleDateFormat("yyyy-MM-dd");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        date = formatForDateInput.parse(reader.readLine());
+        SimpleDateFormat formatForDateOutput = new SimpleDateFormat("MMM' 'dd', 'yyyy", Locale.ENGLISH);
+        System.out.println(formatForDateOutput.format(date).toUpperCase());
+    }
+}
+*/
+}
