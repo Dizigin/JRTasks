1f541c22b1cf4542ff135fc67fcafc331c446608
Chernov <dizigin@yandex.ru>
2021-04-26 11:54:19 +0300

    Added task1013 Many Constructors

---
+++ src/tasks_10/task1013_Many_Constructors.java
@@ -0,0 +1,93 @@
+package tasks_10;
+
+public class task1013_Many_Constructors {
+/*
+Конструкторы класса Human
+Напиши класс Human с 6 полями.
+Придумай и реализуй 10 различных конструкторов для него.
+
+Требования:
+1. Программа не должна считывать данные с клавиатуры.
+2. В классе Human должно быть 6 полей.
+3. Все поля класса Human должны быть private.
+4. В классе Human должно быть 10 конструкторов.
+5. Все конструкторы класса Human должны быть public.
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public static class Human {
+        // Напишите тут ваши переменные и конструкторы
+        private String firstName;
+        private String lastName;
+        private String middleName;
+        private Date birthDate = new Date();
+        private int gender;
+        private int age;
+
+        public Human (String firstName, String lastName, String middleName){
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.middleName = middleName;
+        }
+        public Human (String firstName, String lastName){
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+        public Human (String firstName, String lastName, Date birthDate){
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.birthDate = birthDate;
+        }
+        public Human (String firstName, String lastName, String middleName, Date birthDate){
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.middleName = middleName;
+            this.birthDate = birthDate;
+        }
+        public Human (String firstName, String lastName, String middleName, Date birthDate, int gender){
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.middleName = middleName;
+            this.birthDate = birthDate;
+            this.gender = gender;
+        }
+        public Human (String firstName, String lastName, String middleName, Date birthDate, int gender, int age){
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.middleName = middleName;
+            this.birthDate = birthDate;
+            this.gender = gender;
+            this.age = age;
+        }
+        public Human (String firstName, String lastName, String middleName, int age){
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.middleName = middleName;
+            this.age = age;
+        }
+        public Human (String firstName, String lastName, Date birthDate, int gender){
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.birthDate = birthDate;
+            this.gender = gender;
+        }
+        public Human (String firstName, String lastName, Date birthDate, int gender, int age){
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.birthDate = birthDate;
+            this.gender = gender;
+            this.age = age;
+        }
+        public Human (String firstName, String lastName, int gender, int age){
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.gender = gender;
+            this.age = age;
+        }
+    }
+}
+
+ */
+}
