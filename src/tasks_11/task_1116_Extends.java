687885faae2fbbcb5af16d96da2b93e78d054b53
Chernov <dizigin@yandex.ru>
2021-08-09 17:54:54 +0300

    Added task1116

---
+++ src/tasks_11/task_1116_Extends.java
@@ -0,0 +1,42 @@
+package tasks_11;
+
+public class task_1116_Extends {
+/*
+Цепочка наследования
+Расставь правильно «цепочку наследования» в классах: Pet (домашнее животное), Cat (кот), Dog (собака).
+
+Requirements:
+1. В классе Solution должен быть public класс Pet (домашнее животное).
+2. В классе Solution должен быть public класс Cat (кот).
+3. В классе Solution должен быть public класс Dog (собака).
+4. Правильно унаследуй класс Cat.
+5. Правильно унаследуй класс Dog.
+
+Цепочка наследования
+Расставь правильно «цепочку наследования» в классах: Pet (домашнее животное), Cat (кот), Dog (собака).
+
+Requirements:
+1. В классе Solution должен быть public класс Pet (домашнее животное).
+2. В классе Solution должен быть public класс Cat (кот).
+3. В классе Solution должен быть public класс Dog (собака).
+4. Правильно унаследуй класс Cat.
+5. Правильно унаследуй класс Dog.
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet{
+
+    }
+
+    public class Dog extends Pet{
+
+    }
+}
+ */
+}
