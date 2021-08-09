9fbb7a9b615509ed6ccb6587e572fb436ffdaefc
Chernov <dizigin@yandex.ru>
2021-08-09 17:11:06 +0300

    Added task1111

---
+++ src/tasks_11/task_1111_Extends.java
@@ -0,0 +1,21 @@
+package tasks_11;
+
+public class task_1111_Extends {
+/*
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    //Адам
+    public class Adam {
+
+    }
+
+    //Ева
+    public class Eve extends Adam {
+
+    }
+}
+ */
+
+}
