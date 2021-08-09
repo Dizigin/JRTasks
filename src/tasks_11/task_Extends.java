67aad8579a4749d5295b8c6da87a34400059c6a5
Chernov <dizigin@yandex.ru>
2021-08-09 17:13:34 +0300

    Added task1112

---
+++ src/tasks_11/task_Extends.java
@@ -0,0 +1,19 @@
+package tasks_11;
+
+public class task_Extends {
+/*
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Apple {
+
+    }
+
+    public class IPhone extends Apple {
+
+    }
+}
+
+ */
+}
