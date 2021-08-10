6257c9dc883d6aa3afa1a9994aeb21285080d5bd
Chernov <dizigin@yandex.ru>
2021-08-10 09:45:35 +0300

    Added task1121

---
+++ src/tasks_11/task_1121_Extends.java
@@ -0,0 +1,40 @@
+package tasks_11;
+
+public class task_1121_Extends {
+/*
+Чужой код, незнакомый
+Исправь наследование в классах Cat, Dog, Pet, House, Airplane, чтобы программа компилировалась и работала.
+
+Requirements:
+1. Исправь, если необходимо, наследование в классе Pet.
+2. Исправь, если необходимо, наследование в классе Cat.
+3. Исправь, если необходимо, наследование в классе Dog.
+4. Исправь, если необходимо, наследование в классе House.
+5. Исправь, если необходимо, наследование в классе Airplane.
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet {
+
+    }
+
+    public class Dog extends Pet {
+
+    }
+
+    public class House {
+
+    }
+
+    public class Airplane {
+
+    }
+}
+ */
+}
