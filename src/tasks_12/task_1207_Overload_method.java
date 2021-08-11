d705688114a828ef4f21c69abbdb776e60302db4
Chernov <dizigin@yandex.ru>
2021-08-11 18:26:19 +0300

    Added task1207

---
+++ src/tasks_12/task_1207_Overload_method.java
@@ -0,0 +1,33 @@
+package tasks_12;
+
+public class task_1207_Overload_method {
+/*
+Int и Integer
+Реализуй два метода: print(int) и print(Integer).
+Напиши такой код в методе main(), чтобы вызвались оба.
+
+Requirements:
+1. Класс Solution должен содержать статический метод main().
+2. Класс Solution должен содержать статический void метод print() с параметром типа int.
+3. Класс Solution должен содержать статический void метод print() с параметром типа Integer.
+4. Метод main() должен вызывать метод print() с параметром типа int.
+5. Метод main() должен вызывать метод print() с параметром типа Integer.
+
+public class Solution {
+    public static void main(String[] args) {
+        print(10);
+        print((Integer)20);
+    }
+
+    //Напишите тут ваши методы
+
+
+    static void print (int i){
+
+    }
+    static void print (Integer i){
+
+    }
+}
+ */
+}
