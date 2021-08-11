0a990daac767d1d3b4ebcb38267aa45a148b4a5d
Chernov <dizigin@yandex.ru>
2021-08-11 17:52:19 +0300

    Added task1206

---
+++ src/tasks_12/task_1206_Overload_metod.java
@@ -0,0 +1,29 @@
+package tasks_12;
+
+public class task_1206_Overload_metod {
+/*
+Выполняем перегрузку!
+Напиши два метода: print(int) и print(String).
+
+
+Requirements:
+1. Программа не должна выводить текст на экран.
+2. Класс Solution должен содержать три метода.
+3. Класс Solution должен содержать статический метод main().
+4. Класс Solution должен содержать статический метод print() с параметром типа int.
+5. Класс Solution должен содержать статический метод print() с параметром типа String.
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    static void print(int i){
+    }
+    static void print (String s){
+
+    }
+}
+ */
+}
