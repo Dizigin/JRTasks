47de3df51ae387dcea065ea07ce832e9f931592d
Chernov <dizigin@yandex.ru>
2021-08-11 18:32:49 +0300

    Added task1208

---
+++ src/tasks_12/task_1208_Overload_method.java
@@ -0,0 +1,28 @@
+package tasks_12;
+
+public class task_1208_Overload_method {
+/*
+Свобода печати
+Напиши пять методов print() с разными параметрами.
+
+Requirements:
+1. Программа не должна выводить текст на экран.
+2. Класс Solution должен содержать шесть методов.
+3. Класс Solution должен содержать статический метод main().
+4. Класс Solution должен содержать пять статических методов print() с разными параметрами.
+
+ public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    static void print (int i){}
+    static void print (String s){}
+    static void print (double d){}
+    static void print (int i, double d){}
+    static void print (int i, String s){}
+}
+
+ */
+}
