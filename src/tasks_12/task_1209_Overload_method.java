bc86d4e7f6f7532fe3adff6023537f86de74e1fb
Chernov <dizigin@yandex.ru>
2021-08-11 18:41:44 +0300

    Added task1209

---
+++ src/tasks_12/task_1209_Overload_method.java
@@ -0,0 +1,40 @@
+package tasks_12;
+
+public class task_1209_Overload_method {
+/*
+Три метода и минимум
+Напиши public static методы: int min(int, int), long min(long, long), double min(double, double).
+Каждый метод должен возвращать минимальное из двух переданных в него чисел.
+
+Requirements:
+1. Программа не должна выводить текст на экран.
+2. Класс Solution должен содержать четыре метода.
+3. Класс Solution должен содержать статический метод int min(int, int).
+4. Метод int min(int, int) должен возвращать минимальное из двух чисел типа int.
+5. Класс Solution должен содержать статический метод long min(long, long).
+6. Метод long min(long, long) должен возвращать минимальное из двух чисел типа long.
+7. Класс Solution должен содержать статический метод double min(double, double).
+8. Метод double min(double, double) должен возвращать минимальное из двух чисел типа double.
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    static int min(int x, int y){
+        if (x < y) return x;
+        else return y;
+    }
+    static long min(long x, long y){
+        if (x < y) return x;
+        else return y;
+    }
+    static double min(double x, double y){
+        if (x < y) return x;
+        else return y;
+    }
+
+}
+ */
+}
