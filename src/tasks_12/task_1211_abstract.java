4d8f4443cf837c46c7ba31492a08f40fddfae61c
Chernov <dizigin@yandex.ru>
2021-08-12 18:03:29 +0300

    Added task1211

---
+++ src/tasks_12/task_1211_abstract.java
@@ -0,0 +1,27 @@
+package tasks_12;
+
+public class task_1211_abstract {
+/*
+Абстрактный класс Pet
+Сделай класс Pet абстрактным.
+
+Requirements:
+1. Программа не должна выводить текст на экран.
+2. Класс Pet должен быть статическим.
+3. Класс Pet должен быть абстрактным.
+4. Класс Pet должен иметь один метод getName().
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static abstract class Pet {
+        public String getName() {
+            return "Я - котенок";
+        }
+    }
+
+}
+ */
+}
