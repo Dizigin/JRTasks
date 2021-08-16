e092e317f7c727d772a5c52cb74007ffef173244
Chernov <dizigin@yandex.ru>
2021-08-16 15:49:05 +0300

    Added task1216

---
+++ src/tasks_12/task1216_Interface.java
@@ -0,0 +1,27 @@
+package tasks_12;
+
+public class task1216_Interface {
+/*
+Летать охота
+Напиши свой public интерфейс CanFly(летать).
+Добавь в него два метода.
+
+Requirements:
+1. Класс Solution должен содержать интерфейс CanFly.
+2. Интерфейс CanFly должен быть публичным.
+3. Интерфейс CanFly должен содержать два метода.
+4. У интерфейса CanFly не должно быть родителя.
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //add an interface here - добавь интерфейс тут
+    public static interface CanFly{
+        void takeoff ();
+        void landing ();
+    }
+}
+ */
+}
