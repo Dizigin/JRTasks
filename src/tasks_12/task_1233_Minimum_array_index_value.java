7079208c6ff58014e540faee17885f8f58054aea
Chernov <dizigin@yandex.ru>
2021-08-19 17:57:14 +0300

    Added task1233

---
+++ src/tasks_12/task_1233_Minimum_array_index_value.java
@@ -0,0 +1,50 @@
+package tasks_12;
+
+public class task_1233_Minimum_array_index_value {
+/*
+Изоморфы наступают
+Напиши метод, который возвращает минимальное число в массиве и его позицию (индекс).
+
+Requirements:
+1. Класс Solution должен содержать класс Pair.
+2. Класс Solution должен содержать два метода.
+3. Класс Solution должен содержать метод getMinimumAndIndex().
+4. Метод getMinimumAndIndex() должен возвращать минимальное число в массиве и его позицию (индекс).
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        int[] data = new int[]{1, 2, 3, 5, -2, -8, 0, 77, 5, 5};
+
+        Pair<Integer, Integer> result = getMinimumAndIndex(data);
+
+        System.out.println("The minimum is " + result.x);
+        System.out.println("The index of the minimum element is " + result.y);
+    }
+
+    public static Pair<Integer, Integer> getMinimumAndIndex(int[] array) {
+        if (array == null || array.length == 0) {
+            return new Pair<Integer, Integer>(null, null);
+        }
+
+        //напишите тут ваш код
+        int minIndex = 0;
+        for (int i = 0; i < array.length - 1; i++){
+            if (array[i + 1] < array[minIndex]) minIndex = i + 1;
+        }
+
+        return new Pair<Integer, Integer>(array[minIndex], minIndex);
+    }
+
+
+    public static class Pair<X, Y> {
+        public X x;
+        public Y y;
+
+        public Pair(X x, Y y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
+ */
+}
