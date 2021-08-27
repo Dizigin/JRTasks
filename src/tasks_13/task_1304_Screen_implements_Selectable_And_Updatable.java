3317fc13712250a44de1273a79c2e371734103e4
Chernov <dizigin@yandex.ru>
2021-08-27 15:08:53 +0300

    Added task1304

---
+++ src/tasks_13/task_1304_Screen_implements_Selectable_And_Updatable.java
@@ -0,0 +1,40 @@
+package tasks_13;
+
+public class task_1304_Screen_implements_Selectable_And_Updatable {
+/*
+Selectable и Updatable
+Создай класс Screen и реализуй в нем интерфейсы Selectable и Updatable. Не забудь реализовать методы!
+
+Requirements:
+1. Класс Screen должен реализовывать (implements) интерфейс Selectable.
+2. Класс Screen должен реализовывать (implements) интерфейс Updatable.
+3. В классе Screen должны быть реализованы методы интерфейса Selectable.
+4. В классе Screen должны быть реализованы методы интерфейса Updatable.
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    interface Selectable {
+        void onSelect();
+    }
+
+    interface Updatable {
+        void refresh();
+    }
+
+    //напишите тут ваш класс
+    public static class Screen implements Selectable, Updatable{
+        @Override
+        public void onSelect() {
+
+        }
+
+        @Override
+        public void refresh() {
+
+        }
+    }
+}
+ */
+}
