e52b176f60faee30562b2af4a38ff39fe1414750
Chernov <dizigin@yandex.ru>
2021-09-02 17:54:12 +0300

    Added task1308

---
+++ src/tasks_13/task_1308_interface_extands_interface.java
@@ -0,0 +1,31 @@
+package tasks_13;
+
+public class task_1308_interface_extands_interface {
+/*
+Эй, ты там живой?
+В этой задаче тебе нужно:
+Создать интерфейс Person.
+Добавить в него метод isAlive(), который проверяет, жив человек или нет.
+Подумать, какой тип должен возвращать этот метод.
+Создать интерфейс Presentable.
+Унаследовать интерфейс Presentable от интерфейса Person.
+
+Requirements:
+1. В классе Solution должен быть объявлен интерфейс Person.
+2. В классе Solution должен быть объявлен интерфейс Presentable.
+3. Интерфейс Presentable должен наследоваться от интерфейса Person.
+4. В интерфейсе Person должен быть объявлен метод isAlive.
+5. Тип возвращаемого значения метода isAlive может иметь только два значения.
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+    interface Person {
+        Boolean isAlive();
+    }
+    interface Presentable extends Person {
+
+    }
+}
+ */
+}
