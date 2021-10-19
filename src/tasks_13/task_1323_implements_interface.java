9471ecca2e6ec69cb0810638d034796999915bd8
Chernov <dizigin@yandex.ru>
2021-10-19 21:45:58 +0300

    Added task1323

---
+++ src/tasks_13/task_1323_implements_interface.java
@@ -0,0 +1,39 @@
+package tasks_13;
+
+public class task_1323_implements_interface {
+/*
+Интерфейс Updatable в классе Screen
+Реализуй интерфейс Updatable в классе Screen.
+
+Requirements:
+1. Интерфейс Updatable должен наследовать (extends) интерфейс Selectable.
+2. Класс Screen должен реализовывать (implements) интерфейс Updatable.
+3. В классе Screen должен быть реализован метод onSelect интерфейса Selectable.
+4. В классе Screen должен быть реализован метод refresh интерфейса Updatable.
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    interface Selectable {
+        void onSelect();
+    }
+
+    interface Updatable extends Selectable {
+        void refresh();
+    }
+
+    class Screen implements Updatable{
+        @Override
+        public void refresh() {
+
+        }
+
+        @Override
+        public void onSelect() {
+
+        }
+    }
+}
+ */
+}
