a0b30e4ec47e73c17f16c4df84434586ac6ae30d
Chernov <dizigin@yandex.ru>
2021-10-22 11:50:43 +0300

    Added task1324

---
+++ src/tasks_13/task_1324_implements_interface.java
@@ -0,0 +1,33 @@
+package tasks_13;
+
+public class task_1324_implements_interface {
+/*
+Один метод в классе
+Реализуй интерфейс Animal в классе Fox.
+Поменяй код так, чтобы в классе Fox был только один метод - getName.
+Учти, что методы удалять нельзя.
+
+Requirements:
+1. Интерфейс Animal должен быть реализован в классе Fox.
+2. В классе Fox должен быть реализован только один метод — getName.
+3. Интерфейс Animal изменять нельзя.
+4. Дополнительные классы или интерфейсы создавать нельзя.
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    public interface Animal {
+        Color getColor();
+
+        Integer getAge();
+    }
+
+    public abstract static class Fox implements Animal{
+        public String getName() {
+            return "Fox";
+        }
+    }
+}
+ */
+}
