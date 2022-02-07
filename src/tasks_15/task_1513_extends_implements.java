4fbf8275ffcd470496d94fc36981bb436d4c2e13
Chernov <dizigin@yandex.ru>
2022-02-07 14:41:54 +0300

    Added task1513

---
+++ src/tasks_15/task_1513_extends_implements.java
@@ -0,0 +1,38 @@
+package tasks_15;
+
+public class task_1513_extends_implements {
+/*
+Максимально простой код-3
+Упрости код:
+убери все наследования классов, которые автоматически добавятся при компиляции;
+убери все конструкторы, которые создаются и добавляются автоматически.
+
+Requirements:
+1. В классе Car не должно быть явно реализованных конструкторов без параметров.
+2. В классе Machine не должно быть явно реализованных конструкторов без параметров.
+3. Класс Car должен быть потомком класса Machine.
+4. В коде не должно быть явного наследования от Object (extends Object).
+5. Класс Car не должен явно наследовать интерфейс Runnable.
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public interface Runnable {
+
+    }
+
+    public class Machine implements Runnable {
+        //public Machine() {
+            //super();
+        //}
+    }
+
+    public class Car extends Machine {
+        //public Car() {
+            //super();
+        //}
+    }
+}
+ */
+}
