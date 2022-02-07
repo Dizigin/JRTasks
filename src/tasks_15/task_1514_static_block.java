c6a57074aad2207699fb8a583d8b566a7df685db
Chernov <dizigin@yandex.ru>
2022-02-07 18:19:57 +0300

    Added task1514

---
+++ src/tasks_15/task_1514_static_block.java
@@ -0,0 +1,31 @@
+package tasks_15;
+
+public class task_1514_static_block {
+/*
+Статики-1
+В статическом блоке инициализируй labels пятью различными парами ключ-значение.
+
+Requirements:
+1. В классе Solution должен быть только один метод — main().
+2. В классе Solution должно быть объявлено статическое поле labels типа Map.
+3. Поле labels должно быть заполнено 5 различными парами ключ-значение в статическом блоке.
+4. Метод main должен выводить содержимое labels на экран.
+
+public class Solution {
+    public static Map<Double, String> labels = new HashMap<Double, String>();
+    static
+    {
+        labels.put(1.0, "Первый");
+        labels.put(2.0, "Второй");
+        labels.put(3.0, "Третий");
+        labels.put(4.0, "Четвертый");
+        labels.put(5.0, "Пятый");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(labels);
+    }
+}
+
+ */
+}
